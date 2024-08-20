6472c57d037d26050eca10aebfb8e4e440863043
Aaron Santana <santanavaldelomaraa@gmail.com>
2024-08-19 21:11:58 -0600

    feat: Add functionality for grandchild threads to buy lottery tickets
    This commit adds the necessary code to the `main.c` file to create two grandchild threads that buy lottery tickets. The `buyLottery` function is implemented to generate a random number between 0 and 99, and the numbers obtained by each grandchild thread are printed in the main thread. This enhances the functionality of the Grandma Lottery exercise.

--- exercises/pthreads/grandma_lottery/src/main.c
+++ exercises/pthreads/grandma_lottery/src/main.c
@@ -2,28 +2,41 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-void* greet(void* data);
+void* buyLottery(void* data);
 
 // procedure main:
 int main(void) {
-  // create_thread(greet)
-  pthread_t thread;
-  int error = pthread_create(&thread, /*attr*/ NULL, greet, /*arg*/ NULL);
-  if (error == EXIT_SUCCESS) {
-    // print "Hello from main thread"
-    printf("Hello from main thread\n");
-    pthread_join(thread, /*value_ptr*/ NULL);
-  } else {
-    fprintf(stderr, "Error: could not create secondary thread\n");
+  // create thwo grandchild threads who buy lottery tickets
+  pthread_t grandchild1, grandchild2;
+  int error = pthread_create(&grandchild1, /*attr*/ NULL,
+      buyLottery, /*arg*/ NULL);
+  int error2 = pthread_create(&grandchild2, /*attr*/ NULL,
+      buyLottery, /*arg*/ NULL);
+  if (error == EXIT_FAILURE || error2 == EXIT_FAILURE) {
+    printf("Error creating thread\n");
+    return EXIT_FAILURE;
   }
-  return error;
+
+  // Wait for the threads to finish
+  void* number1 = NULL;
+  void* number2 = NULL;
+  pthread_join(grandchild1, /*value_ptr*/ &number1);
+  pthread_join(grandchild2, /*value_ptr*/ &number2);
+
+  // report the numbers that the grandchild threads got
+  printf("Grandchild 1 got number %zu\n", (size_t) number1);
+  printf("Grandchild 2 got number %zu\n", (size_t) number2);
+
+  return EXIT_SUCCESS;
 }  // end procedure
 
-// procedure greet:
-void* greet(void* data) {
-  (void)data;
-  // print "Hello from secondary thread"
-  printf("Hello from secondary thread\n");
-  return NULL;
+// procedure buyLottery:
+void* buyLottery(void* data) {
+  (void) data;
+  unsigned int mySeed = time(NULL) + clock();
+  size_t number = rand_r(&mySeed) % 100;
+  //  return &number;
+  return (void*)number;
 }  // end procedure
