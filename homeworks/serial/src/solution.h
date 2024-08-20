9270ee62090a95ccc46d84bafe3a439305e4f11d
Aaron Santana <santanavaldelomaraa@gmail.com>
2024-08-20 12:10:07 -0600

    fix: linter issues

--- homeworks/serial/src/solution.h
+++ homeworks/serial/src/solution.h
@@ -1,9 +1,10 @@
+// Copyright [2024] <Aaron Santana Valdelomar>
+
 #pragma once
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct
-{
+typedef struct {
     char* jobFile;
     size_t threadsCount;
 } Arguments;
