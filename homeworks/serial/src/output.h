7983943a36839e60c402666a2b3533f9d8f9c730
Aaron Santana <santanavaldelomaraa@gmail.com>
2024-08-23 22:51:17 -0600

    doc: output functions

--- homeworks/serial/src/output.h
+++ homeworks/serial/src/output.h
@@ -10,7 +10,22 @@
  */
 void writePlate(Plate plate, const char* binaryFilpath);
 
+/**
+ * Writes the results of the jobs to a file.
+ *
+ * @param jobsData The array of JobData containing the job information.
+ * @param results The array of SimulationResult containing the simulation results.
+ * @param jobsCount The number of jobs.
+ * @param filepath The path of the file to write the results to.
+ */
 void writeJobsResult(JobData* jobsData, SimulationResult* results, size_t jobsCount, const char* filepath);
+/**
+ * Writes the result of a job to a file.
+ *
+ * @param jobData The data of the job.
+ * @param result The simulation result.
+ * @param file The file to write the result to.
+ */
 void writeJobResult(JobData jobData, SimulationResult result, FILE* file);
 
 /**
@@ -20,4 +35,11 @@ void writeJobResult(JobData jobData, SimulationResult result, FILE* file);
  */
 void printPlate(Plate plate);
 
+/**
+ * Formats the given time in seconds into a human-readable (YYYY/MM/DD HH:MM:SS) format and stores it in the provided buffer.
+ *
+ * @param seconds The time in seconds to be formatted.
+ * @param buffer The buffer to store the formatted time.
+ * @param buffer_size The size of the buffer.
+ */
 void format_time(time_t seconds, char *buffer, size_t buffer_size);
