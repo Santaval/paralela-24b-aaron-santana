0793b8cda6e6a5fcbfba4681ff2c7e5e388ef1ab
= <santanavaldelomaraa@gmail.com>
2024-08-27 11:35:22 -0600

    refactor: Update create_threads function to accept a routine and data parameter

--- examples/pthreads/create_thread_team/src/main.c
+++ examples/pthreads/create_thread_team/src/main.c
@@ -9,63 +9,76 @@
 struct private_data {
   size_t thread_number;  // rank
   size_t thread_count;
+  void* data;
+ pthread_t thread_id;
 };
 
 void* greet(void* data);
-int create_threads(size_t thread_count);
+struct private_data*  create_threads(size_t thread_count, void* (*routine)(void* data), void* data);
+int join_threads(size_t thread_count, struct private_data* team);
 
 // procedure main(argc, argv[])
 int main(int argc, char* argv[]) {
-  int error = EXIT_SUCCESS;
+  struct private_data* team = NULL;
   size_t thread_count = sysconf(_SC_NPROCESSORS_ONLN);
   if (argc == 2) {
     if (sscanf(argv[1], "%zu", &thread_count) != 1) {
       fprintf(stderr, "Error: invalid thread count\n");
-      return 11;
+      return EXIT_FAILURE;
     }
   }
 
-  error = create_threads(thread_count);
-  return error;
+  team = create_threads(thread_count, greet, NULL);
+  if (team) {
+  // print "Hello from main thread"
+  printf("Hello from main thread\n");
+  join_threads(thread_count, team);
+
+  } else {
+    fprintf(stderr, "Error: creating threads team\n");
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
 }  // end procedure
 
 
-int create_threads(size_t thread_count) {
-  int error = EXIT_SUCCESS;
+struct private_data* create_threads(size_t thread_count, void* (*routine)(void* data), void* data) {
   // for thread_number := 0 to thread_count do
-  pthread_t* threads = (pthread_t*) malloc(thread_count * sizeof(pthread_t));
-  struct private_data* private_data = (struct private_data*)
+  struct private_data* team = (struct private_data*)
     calloc(thread_count, sizeof(struct private_data));
-  if (threads && private_data) {
+  if (team) {
     for (size_t thread_number = 0; thread_number < thread_count
         ; ++thread_number) {
-      private_data[thread_number].thread_number = thread_number;
-      private_data[thread_number].thread_count = thread_count;
-      // create_thread(greet, thread_number)
-      if (pthread_create(&threads[thread_number], NULL, greet
-        , &private_data[thread_number]) != EXIT_SUCCESS) {
+      team[thread_number].thread_number = thread_number;
+      team[thread_number].thread_count = thread_count;
+      team[thread_number].data = data;
+      // create_thread(routine, thread_number)
+      if (pthread_create(&team[thread_number].thread_id, NULL, routine
+        , &team[thread_number]) != EXIT_SUCCESS) {
         fprintf(stderr, "Error: could not create secondary thread\n");
-        error = EXIT_FAILURE;
-        break;
+        join_threads(thread_number, team);
+        return NULL;
       }
     }
-
-    // print "Hello from main thread"
-    printf("Hello from main thread\n");
-
-    for (size_t thread_number = 0; thread_number < thread_count
-        ; ++thread_number) {
-      pthread_join(threads[thread_number], NULL);
-    }
-
-    free(private_data);
-    free(threads);
   } else {
     fprintf(stderr, "Error: could not allocate %zu threads\n", thread_count);
-    error = EXIT_FAILURE;
+    return NULL;
   }
 
-  return error;
+  return team;
+}
+
+int join_threads(const size_t thread_count, struct private_data* team) {
+  int result = EXIT_SUCCESS;
+  for (size_t thread_number = 0; thread_number < thread_count
+        ; ++thread_number) {
+      int error = pthread_join(team[thread_number].thread_id, NULL);
+      if (result == EXIT_SUCCESS) {
+       result = error;
+      }
+    }
+    free(team);
+    return result;
 }
 
 // procedure greet:
